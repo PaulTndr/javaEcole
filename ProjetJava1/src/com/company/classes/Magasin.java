9d04d1da3167ae6732572b78684459feb1ea8b33
Paul TONDEREAU <paul.tondereau@yahoo.fr>
2019-09-23 15:01:11 +0200

    Clean archi + création classes manquantes

---
+++ ProjetJava1/src/com/company/classes/Magasin.java
@@ -0,0 +1,10 @@
+package com.company.classes;
+import java.util.*;
+
+public class Magasin {
+    public String nom;
+    public String adresse;
+    public int nbrVendeurs;
+    public ArrayList<HashSet<String>> listArticles;
+    public int caisse;
+}
---
+++ ProjetJava1/src/com/company/interfaces/ISolde.java
@@ -0,0 +1,8 @@
+package com.company.interfaces;
+
+public interface ISolde {
+
+    public void printTaux();
+    public void lancerSoldes();
+    public void stopSoldes();
+}
